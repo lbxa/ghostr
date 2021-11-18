80aefacecc37ce1f3e02001f52d71bc59b7a63e8
Lucas Barbosa <lucas.chu.barbosa@gmail.com>
2021-11-19 03:12:22 +1100

    blocking functionality is working along with newer stable version.

--- helper.py
+++ helper.py
@@ -2,12 +2,14 @@ from user import User
 
 
 def check_unicast_block(sender, receiver):
-    if User().is_blocked_by(blocker=receiver, blockee=sender):
-        # sender blocked by receiver
-        return f"TYPE: BLOCK;;WHO: {sender};;BLOCKER: {receiver};;RET: 0;;ERR: {receiver} has blocked you;;"
-    else:
-        return False
+    # sender blocked by receiver
+    err_msg = f"TYPE: BLOCK;;WHO: {sender};;BLOCKER: {receiver};;RET: 0;;ERR: {receiver} has blocked you;;"
+    return err_msg if User().is_blocked_by(blocker=receiver, blockee=sender) else False
 
 
-# def check_broadcast_block(sender):
-#     user_directory = User().get_all_users()
+def check_broadcast_block(sender):
+    err_txt = "Your message could not be delivered to some recipients"
+    for user in User().get_all_users():
+        if User().is_blocked_by(blocker=user, blockee=sender):
+            return f"TYPE: BLOCK;;WHO: {sender};;BLOCKER: {user};;RET: 0;;ERR: {err_txt};;"
+    return False
