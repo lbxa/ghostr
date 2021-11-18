77b8eaebc1a9f770242ebd9b34a70d25d6afa02a
Lucas Barbosa <lucas.chu.barbosa@gmail.com>
2021-11-19 08:00:34 +1100

    adding working version

---
+++ v2/UserManager.py
@@ -0,0 +1,215 @@
+# Python 3.7
+# Author: Bofei Wang
+# coding: utf-8
+# this file contains the UserManager class for the server to use
+
+from typing import Dict, Set
+from time import time
+
+
+class UserManager:
+    # manage all user data, including credentials, block status,
+    # user-to-user block status, time out, online status,
+    # and last login status
+
+    def __init__(self, block_duration: int, time_out: int):
+        self.__user_map: Dict[str, UserManager.__User] = dict()
+        self.__address_to_username_map: Dict[str, str] = dict()
+        self.__username_to_address_map: Dict[str, str] = dict()
+        self.__block_duration: int = block_duration
+        self.__time_out: int = time_out
+        self.__read_credentials()
+
+    def __read_credentials(self):
+        try:
+            with open("credentials.txt", "r") as credential_file:
+                for credential in credential_file:
+                    username, password = credential.strip().split()
+                    self.__user_map[username] = UserManager.__User(username, password,
+                                                                   self.__block_duration,
+                                                                   self.__time_out)
+        except:
+            print("FATAL: error reading credentials.txt")
+            exit(1)
+
+    def authenticate(self, username_input: str, password_input: str):
+        # authenticate user and update status
+        # return updated status in a string format
+
+        if username_input not in self.__user_map:
+            # username unknown
+            return "USERNAME_NOT_EXIST"
+
+        # else, delegate authenticate to specific user class
+        return self.__user_map[username_input].authenticate(password_input)
+
+    def set_address_username(self, address: str, username: str):
+        self.__address_to_username_map[address] = username
+        self.__username_to_address_map[username] = address
+
+    def get_username(self, address: str) -> str:
+        if address in self.__address_to_username_map:
+            return self.__address_to_username_map[address]
+        else:
+            return ""
+
+    def get_address(self, username: str) -> str:
+        if username in self.__username_to_address_map:
+            return self.__username_to_address_map[username]
+        else:
+            return ""
+
+    def set_offline(self, username):
+        if username in self.__user_map:
+            self.__user_map[username].set_offline()
+
+    def update(self):
+        # update all user's block status
+        for user_credential in self.__user_map.values():
+            user_credential.update()
+
+    def block(self, from_username: str, to_block_username: str):
+        if from_username in self.__user_map:
+            self.__user_map[from_username].block(to_block_username)
+
+    def unblock(self, from_username: str, to_block_username: str):
+        if from_username in self.__user_map:
+            self.__user_map[from_username].unblock(to_block_username)
+
+    def is_blocked_user(self, from_username: str, to_block_username: str):
+        # if from user blocked to user
+        return from_username in self.__user_map and self.__user_map[from_username].is_blocked_user(to_block_username)
+
+    def has_user(self, username):
+        return username in self.__user_map
+
+    def is_online(self, username):
+        return username in self.__user_map and self.__user_map[username].is_online()
+
+    def all_users(self) -> list:
+        # return a list of all username
+        return list(self.__user_map.keys())
+
+    def get_timed_out_users(self) -> set:
+        # return a set of all users that should be timed out
+        timed_out_users = set()
+        for user in self.__user_map:
+            if self.__user_map[user].update_time_out():
+                timed_out_users.add(user)
+        return timed_out_users
+
+    def get_online_users(self) -> set:
+        online_users = set()
+        for user in self.__user_map:
+            if self.__user_map[user].is_online():
+                online_users.add(user)
+        return online_users
+
+    def get_users_logged_in_since(self, since: int) -> set:
+        # return a set of username tha is logged in since a time
+        users = set()
+        for user in self.__user_map:
+            if self.__user_map[user].last_log_in() > time() - since:
+                users.add(user)
+        return users
+
+    def refresh_user_timeout(self, username):
+        # update a user's last active time
+        if username in self.__user_map:
+            self.__user_map[username].refresh_user_timeout()
+
+    def set_private_port(self, username: str, port: int):
+        if username in self.__user_map:
+            self.__user_map[username].set_private_port(port)
+
+    def get_private_port(self, username: str):
+        if username in self.__user_map:
+            return self.__user_map[username].get_private_port()
+        return 0
+
+    class __User:
+        # manage username, password, online status, number of consecutive fail trials,
+        # blocked timestamp, last active time, last login of a particular user
+
+        def __init__(self, username: str, password: str, block_duration: int, timeout: int):
+            self.__username: str = username
+            self.__password: str = password
+            self.__block_duration: int = block_duration
+            self.__timeout: int = timeout
+            self.__online: bool = False
+            self.__blocked: bool = False
+            self.__consecutive_fails: int = 0
+            self.__blocked_since: int = 0
+            self.__inactive_since: int = int(time())
+            self.__blocked_users: Set[str] = set()
+            self.__last_login: int = 0
+            self.__private_port: int = 0
+
+        def block(self, username: str):
+            self.__blocked_users.add(username)
+
+        def unblock(self, username: str):
+            if username in self.__blocked_users:
+                self.__blocked_users.remove(username)
+
+        def set_private_port(self, port: int):
+            self.__private_port = port
+
+        def get_private_port(self):
+            return self.__private_port
+
+        def is_blocked_user(self, username: str):
+            return username in self.__blocked_users
+
+        def update(self):
+            # unblock users if any
+            if self.__blocked and self.__blocked_since + self.__block_duration < time():
+                self.__blocked = False
+
+        def set_offline(self):
+            self.__online = False
+            self.__consecutive_fails = 0
+            self.__blocked_since = 0
+            self.__private_port = 0
+
+        def is_online(self):
+            return self.__online
+
+        def update_time_out(self):
+            # update time out status, return true if should lof out this user because of timeout
+            if self.is_online() and self.__inactive_since + self.__timeout < time():
+                self.set_offline()
+                return True
+            return False
+
+        def refresh_user_timeout(self):
+            self.__inactive_since = time()
+
+        def last_log_in(self):
+            return self.__last_login
+
+        def authenticate(self, password_input: str):
+            # authenticate, return the status of the updated user
+
+            if self.__online:
+                # user is already logged in
+                return "ALREADY_LOGGED_IN"
+
+            if self.__blocked:
+                # user is blocked
+                return "BLOCKED"
+
+            if self.__password != password_input:
+                # incorrect password
+                self.__consecutive_fails += 1
+                if self.__consecutive_fails >= 3:
+                    self.__blocked_since = time()
+                    self.__blocked = True
+                    return "INVALID_PASSWORD_BLOCKED"
+                return "INVALID_PASSWORD"
+
+            # is able to login. update status
+            self.__online = True
+            self.__last_login = int(time())
+            self.refresh_user_timeout()
+            return "SUCCESS"
---
+++ v2/server.py
@@ -0,0 +1,302 @@
+# Python 3.7
+# Author: Bofei Wang
+# Usage: python3 server.py server_port block_duration timeout
+# coding: utf-8
+# modified from the multi-threading sample code
+
+import threading
+import time
+import json
+import sys
+import atexit
+import signal
+from socket import *
+from typing import List, Dict
+from UserManager import UserManager
+
+# command line args
+if len(sys.argv) != 4:
+    print("Usage: python3 server.py server_port block_duration timeout")
+    exit(0)
+serverPort = int(sys.argv[1])
+block_duration = int(sys.argv[2])
+timeout = int(sys.argv[3])
+
+# exclusive lock for multi threading
+t_lock = threading.Condition()
+
+# will store clients info in this list
+clients = []
+
+# all unsent messages: from_user, to_user, message
+pending_messages: List[Dict] = []
+
+# map username to connection socket
+name_to_socket: Dict = dict()
+
+# would communicate with clients after every second
+UPDATE_INTERVAL = 1
+
+# user manager manages all the user data
+user_manager = UserManager(block_duration, timeout)
+
+
+# catch the ctrl+c exit signal
+def keyboard_interrupt_handler(signal, frame):
+    print("\rServer is shutdown")
+    exit(0)
+
+
+# close the socket when exit
+def on_close():
+    serverSocket.close()
+
+
+# helper function to send a message
+def send_message(from_user: str, to_user: str, message: str, broadcast=False, login_broadcast=False,
+                 logout_broadcast=False):
+    if to_user not in name_to_socket:
+        print('ERROR', to_user, 'not exist')
+    else:
+        to_user_socket = name_to_socket[to_user]
+        action = 'receive_message'
+        if broadcast:
+            action = 'receive_broadcast'
+        elif login_broadcast:
+            action = 'login_broadcast'
+        elif logout_broadcast:
+            action = 'logout_broadcast'
+        to_user_socket.send(json.dumps({
+            'action': action,
+            'from': from_user,
+            'message': message
+        }).encode())
+
+
+# return a function as connection handler for a specific socket for multi threading
+def connection_handler(connection_socket, client_address):
+    def real_connection_handler():
+        while True:
+            data = connection_socket.recv(1024)
+            if not data:
+                # if data is empty, the socket is closed or is in the
+                # process of closing. In this case, close this thread
+                exit(0)
+
+            # received data from the client, now we know who we are talking with
+            data = data.decode()
+            data = json.loads(data)
+            action = data["action"]
+
+            # get lock as we might me accessing some shared data structures
+            with t_lock:
+                # debugging code, uncomment to use
+                # print(client_address, ':', data)
+
+                # the data to reply to client
+                server_message = dict()
+                server_message["action"] = action
+
+                # current user name
+                curr_user = user_manager.get_username(client_address)
+
+                # update the time out when user send anything to server
+                user_manager.refresh_user_timeout(curr_user)
+
+                if action == 'login':
+                    # store client information (IP and Port No) in list
+                    username = data["username"]
+                    password = data["password"]
+                    clients.append(client_address)
+                    # auth the user and reply the status
+                    status = user_manager.authenticate(username, password)
+                    user_manager.set_address_username(client_address, username)
+                    server_message["status"] = status
+                    if status == 'SUCCESS':
+                        # add the socket to the name-socket map
+                        name_to_socket[username] = connection_socket
+                        user_manager.set_private_port(username, int(data['private_port']))
+                        # broadcast new user login
+                        for user in user_manager.all_users():
+                            if user != username and user_manager.is_online(user):
+                                send_message(username, user, '', login_broadcast=True)
+                elif action == 'logout':
+                    # check if client already subscribed or not
+                    user_manager.set_offline(user_manager.get_username(client_address))
+                    if client_address in clients:
+                        clients.remove(client_address)
+                        server_message["reply"] = "logged out"
+                        # broadcast user logout
+                        for user in user_manager.all_users():
+                            if user != curr_user and user_manager.is_online(user):
+                                send_message(curr_user, user, '', logout_broadcast=True)
+                    else:
+                        server_message["reply"] = "You are not logged in"
+                elif action == 'message':
+                    # user tries to send a message to other users
+                    username = data['user']
+                    message = data['message']
+                    if curr_user == username:
+                        server_message['status'] = 'MESSAGE_SELF'
+                    elif not user_manager.has_user(username):
+                        server_message['status'] = 'USER_NOT_EXIST'
+                    elif user_manager.is_blocked_user(username, curr_user):
+                        server_message['status'] = 'USER_BLOCKED'
+                    else:
+                        server_message['status'] = 'SUCCESS'
+                        if user_manager.is_online(username):
+                            # user is online, send message to user
+                            send_message(curr_user, username, message)
+                        else:
+                            # user is offline, add message to pending list
+                            pending_messages.append({
+                                'from_user': curr_user,
+                                'to_user': username,
+                                'message': message
+                            })
+                elif action == 'broadcast':
+                    # broadcast the message to online unblocked users
+                    # record the statistics
+                    message = data['message']
+                    n_sent = 0
+                    n_blocked = 0
+                    for user in user_manager.all_users():
+                        if user_manager.is_blocked_user(user, curr_user):
+                            n_blocked += 1
+                        elif not user_manager.is_online(user):
+                            pass
+                        elif user == curr_user:
+                            pass
+                        else:
+                            n_sent += 1
+                            send_message(curr_user, user, message, broadcast=True)
+                    server_message['n_sent'] = n_sent
+                    server_message['n_blocked'] = n_blocked
+                elif action == 'block':
+                    user_to_block = data['user']
+                    if curr_user == user_to_block:
+                        server_message['status'] = 'MESSAGE_SELF'
+                    elif not user_manager.has_user(user_to_block):
+                        server_message['status'] = 'USER_NOT_EXIST'
+                    else:
+                        server_message['status'] = 'SUCCESS'
+                        user_manager.block(curr_user, user_to_block)
+                elif action == 'unblock':
+                    user_to_unblock = data['user']
+                    if curr_user == user_to_unblock:
+                        server_message['status'] = 'MESSAGE_SELF'
+                    elif not user_manager.has_user(user_to_unblock):
+                        server_message['status'] = 'USER_NOT_EXIST'
+                    else:
+                        server_message['status'] = 'SUCCESS'
+                        user_manager.unblock(curr_user, user_to_unblock)
+                elif action == 'whoelse':
+                    online_users = user_manager.get_online_users()
+                    # remove the user who requested
+                    online_users.remove(curr_user)
+                    server_message['reply'] = list(online_users)
+                elif action == 'whoelsesince':
+                    users = user_manager.get_users_logged_in_since(int(data['since']))
+                    if curr_user in users:
+                        # remove the user who requested
+                        users.remove(curr_user)
+                    server_message['reply'] = list(users)
+                elif action == 'startprivate':
+                    # return user address and port if available
+                    user = data['user']
+                    if not user_manager.has_user(user):
+                        server_message['reply'] = 'USER_NOT_EXIST'
+                    elif user_manager.is_blocked_user(user, curr_user):
+                        server_message['reply'] = 'USER_BLOCKED'
+                    elif user == curr_user:
+                        server_message['reply'] = 'USER_SELF'
+                    elif not user_manager.is_online(user):
+                        server_message['reply'] = 'USER_OFFLINE'
+                    else:
+                        # can provide user details to user
+                        server_message['reply'] = 'SUCCESS'
+                        user_socket = name_to_socket[user]
+                        user_address = user_socket.getsockname()[0]
+                        server_message['address'] = user_address
+                        server_message['port'] = user_manager.get_private_port(user)
+                        server_message['username'] = user
+                else:
+                    server_message["reply"] = "Unknown action"
+                # send message to the client
+                connection_socket.send(json.dumps(server_message).encode())
+                # notify the thread waiting
+                t_lock.notify()
+
+    return real_connection_handler
+
+
+# handles all incoming data and replies to those
+def recv_handler():
+    global t_lock
+    global clients
+    global serverSocket
+    print('Server is up.')
+    while True:
+        # create a new connection for a new client
+        connection_socket, client_address = serverSocket.accept()
+
+        # create a new function handler for the client
+        socket_handler = connection_handler(connection_socket, client_address)
+
+        # create a new thread for the client socket
+        socket_thread = threading.Thread(name=str(client_address), target=socket_handler)
+        socket_thread.daemon = False
+        socket_thread.start()
+
+
+# handles all out going data that can not be handled by recev_dandler
+def send_handler():
+    global t_lock
+    global clients
+    global serverSocket
+    while True:
+        # get lock
+        with t_lock:
+            # check if any pending messages can be send to any users who is online
+            for message in pending_messages:
+                if user_manager.is_online(message['to_user']):
+                    send_message(message['from_user'], message['to_user'], message['message'])
+                    pending_messages.remove(message)
+            # time out users
+            for user in user_manager.get_timed_out_users():
+                if user in name_to_socket:
+                    user_manager.set_offline(user)
+                    name_to_socket[user].send(json.dumps({
+                        'action': 'timeout'
+                    }).encode())
+            # notify other thread
+            t_lock.notify()
+        # sleep for UPDATE_INTERVAL
+        time.sleep(UPDATE_INTERVAL)
+
+
+# we will use two sockets, one for sending and one for receiving
+serverSocket = socket(AF_INET, SOCK_STREAM)
+serverSocket.bind(('localhost', serverPort))
+serverSocket.listen(1)
+
+recv_thread = threading.Thread(name="RecvHandler", target=recv_handler)
+recv_thread.daemon = True
+recv_thread.start()
+
+send_thread = threading.Thread(name="SendHandler", target=send_handler)
+send_thread.daemon = True
+send_thread.start()
+
+# register keyboard interrupt handler
+signal.signal(signal.SIGINT, keyboard_interrupt_handler)
+
+# register exist handler
+atexit.register(on_close)
+
+# this is the main thread
+while True:
+    time.sleep(0.1)
+
+    # update any information of all user data
+    user_manager.update()
